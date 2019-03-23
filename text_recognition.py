2d4eaca6655a65959c1c8dfd1869ca768a373431
mitkosl <mitkosl@abv.bg>
2019-03-23 12:56:58 +0200

    changes in text recognition

--- text_recognition.py
+++ text_recognition.py
@@ -85,30 +85,7 @@ ap.add_argument("-p", "--padding", type=float, default=0.0,
 	help="amount of padding to add to each border of ROI")
 args = vars(ap.parse_args())
 
-camera = PiCamera()
-stream = io.BytesIO()
-camera.capture(stream, format='jpeg')
-#stream.seek(0)
-#image = Image.open(stream)
-# Construct a numpy array from the stream
-data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-# "Decode" the image from the array, preserving colour
-image = cv2.imdecode(data, 1)
 
-# load the input image and grab the image dimensions
-#image = cv2.imread(args["image"])
-orig = image.copy()
-(origH, origW) = image.shape[:2]
-
-# set the new width and height and then determine the ratio in change
-# for both the width and height
-(newW, newH) = (args["width"], args["height"])
-rW = origW / float(newW)
-rH = origH / float(newH)
-
-# resize the image and grab the new image dimensions
-image = cv2.resize(image, (newW, newH))
-(H, W) = image.shape[:2]
 
 # define the two output layer names for the EAST detector model that
 # we are interested -- the first is the output probabilities and the
@@ -119,80 +96,126 @@ layerNames = [
 
 # load the pre-trained EAST text detector
 print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["east"])
+EASTNet = cv2.dnn.readNet(args["east"])
+print("[INFO] Finnished loading EAST text detector!")
 
-# construct a blob from the image and then perform a forward pass of
-# the model to obtain the two output layer sets
-blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
-	(123.68, 116.78, 103.94), swapRB=True, crop=False)
-net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
 
-# decode the predictions, then  apply non-maxima suppression to
-# suppress weak, overlapping bounding boxes
-(rects, confidences) = decode_predictions(scores, geometry)
-boxes = non_max_suppression(np.array(rects), probs=confidences)
+camera = PiCamera()
+#MAX still pic resolution 2592x1944
+# 1280, 720
+camera.resolution = (640, 360)
+#camera.framerate = 30
+# Wait for the automatic gain control to settle
+#time.sleep(2)
+# Now fix the values
+#camera.shutter_speed = camera.exposure_speed
+#camera.exposure_mode = 'off'
+#g = camera.awb_gains
+#camera.awb_mode = 'on'
+#camera.awb_gains = g
+#Finally, take several photos with the fixed settings
+#camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
 
-# initialize the list of results
-results = []
+while True:
+	stream = io.BytesIO()
+	camera.capture(stream, format='jpeg')
+	# Construct a numpy array from the stream
+	data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+	# "Decode" the image from the array, preserving colour
+	image = cv2.imdecode(data, 1)
 
-# loop over the bounding boxes
-for (startX, startY, endX, endY) in boxes:
-	# scale the bounding box coordinates based on the respective
-	# ratios
-	startX = int(startX * rW)
-	startY = int(startY * rH)
-	endX = int(endX * rW)
-	endY = int(endY * rH)
+	# load the input image and grab the image dimensions
+	#image = cv2.imread(args["image"])
+	orig = image.copy()
+	(origH, origW) = image.shape[:2]
 
-	# in order to obtain a better OCR of the text we can potentially
-	# apply a bit of padding surrounding the bounding box -- here we
-	# are computing the deltas in both the x and y directions
-	dX = int((endX - startX) * args["padding"])
-	dY = int((endY - startY) * args["padding"])
+	# set the new width and height and then determine the ratio in change
+	# for both the width and height
+	(newW, newH) = (args["width"], args["height"])
+	rW = origW / float(newW)
+	rH = origH / float(newH)
 
-	# apply padding to each side of the bounding box, respectively
-	startX = max(0, startX - dX)
-	startY = max(0, startY - dY)
-	endX = min(origW, endX + (dX * 2))
-	endY = min(origH, endY + (dY * 2))
+	# resize the image and grab the new image dimensions
+	image = cv2.resize(image, (newW, newH))
+	(H, W) = image.shape[:2]
 
-	# extract the actual padded ROI
-	roi = orig[startY:endY, startX:endX]
+	# construct a blob from the image and then perform a forward pass of
+	# the model to obtain the two output layer sets
+	blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+		(123.68, 116.78, 103.94), swapRB=True, crop=False)
+	EASTNet.setInput(blob)
+	(scores, geometry) = EASTNet.forward(layerNames)
 
-	# in order to apply Tesseract v4 to OCR text we must supply
-	# (1) a language, (2) an OEM flag of 4, indicating that the we
-	# wish to use the LSTM neural net model for OCR, and finally
-	# (3) an OEM value, in this case, 7 which implies that we are
-	# treating the ROI as a single line of text
-	config = ("-l eng --oem 1 --psm 7")
-	text = pytesseract.image_to_string(roi, config=config)
+	# decode the predictions, then  apply non-maxima suppression to
+	# suppress weak, overlapping bounding boxes
+	(rects, confidences) = decode_predictions(scores, geometry)
+	boxes = non_max_suppression(np.array(rects), probs=confidences)
 
-	# add the bounding box coordinates and OCR'd text to the list
-	# of results
-	results.append(((startX, startY, endX, endY), text))
+	# initialize the list of results
+	results = []
 
-# sort the results bounding box coordinates from top to bottom
-results = sorted(results, key=lambda r:r[0][1])
+	# loop over the bounding boxes
+	for (startX, startY, endX, endY) in boxes:
+		# scale the bounding box coordinates based on the respective
+		# ratios
+		startX = int(startX * rW)
+		startY = int(startY * rH)
+		endX = int(endX * rW)
+		endY = int(endY * rH)
 
-# loop over the results
-for ((startX, startY, endX, endY), text) in results:
-	# display the text OCR'd by Tesseract
-	print("OCR TEXT")
-	print("========")
-	print("{}\n".format(text))
-	s.speak(text)
+		# in order to obtain a better OCR of the text we can potentially
+		# apply a bit of padding surrounding the bounding box -- here we
+		# are computing the deltas in both the x and y directions
+		dX = int((endX - startX) * args["padding"])
+		dY = int((endY - startY) * args["padding"])
 
-	# strip out non-ASCII text so we can draw the text on the image
-	# using OpenCV, then draw the text and a bounding box surrounding
-	# the text region of the input image
-	text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
-	output = orig.copy()
-	cv2.rectangle(output, (startX, startY), (endX, endY),
-		(0, 0, 255), 2)
-	cv2.putText(output, text, (startX, startY - 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+		# apply padding to each side of the bounding box, respectively
+		startX = max(0, startX - dX)
+		startY = max(0, startY - dY)
+		endX = min(origW, endX + (dX * 2))
+		endY = min(origH, endY + (dY * 2))
 
-	# show the output image
-	cv2.imshow("Text Detection", output)
+		# extract the actual padded ROI
+		roi = orig[startY:endY, startX:endX]
+
+		# in order to apply Tesseract v4 to OCR text we must supply
+		# (1) a language, (2) an OEM flag of 4, indicating that the we
+		# wish to use the LSTM neural net model for OCR, and finally
+		# (3) an OEM value, in this case, 7 which implies that we are
+		# treating the ROI as a single line of text
+		config = ("-l eng --oem 1 --psm 7")
+		text = pytesseract.image_to_string(roi, config=config)
+
+		# add the bounding box coordinates and OCR'd text to the list
+		# of results
+		results.append(((startX, startY, endX, endY), text))
+
+	# sort the results bounding box coordinates from top to bottom
+	results = sorted(results, key=lambda r:r[0][1])
+
+	# loop over the results
+	for ((startX, startY, endX, endY), text) in results:
+		# display the text OCR'd by Tesseract
+		print("OCR TEXT")
+		print("========")
+		print("{}\n".format(text))
+		s.speak(text)
+
+		# strip out non-ASCII text so we can draw the text on the image
+		# using OpenCV, then draw the text and a bounding box surrounding
+		# the text region of the input image
+		text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+		output = orig.copy()
+		cv2.rectangle(output, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(output, text, (startX, startY - 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+
+		# show the output image
+		cv2.imshow("Text Detection", output)
 	cv2.waitKey(0)
+
+	 key = cv2.waitKey(250) & 0xFF
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+			break
